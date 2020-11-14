c5f77ce9d83b23bdca8e73930c0fe93c69d75a99
Bedoui-islem <islembedoui123@gmail.com>
2020-11-14 22:20:26 +0100

    Composant Accueil finalisé

---
+++ src/app/acceuil/acceuil.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-acceuil',
+  templateUrl: './acceuil.component.html',
+  styleUrls: ['./acceuil.component.css']
+})
+export class AcceuilComponent implements OnInit {
+  nomSupermarket: string = "Chez Nous";
+  lesCategories= [
+    {nom:'informatique', image:'assets/Images/informatique.jpg'},
+    {nom:'alimentation', image:'assets/Images/alimentation.jpg'},
+    {nom:'vetements', image:'assets/Images/habits.png'},
+    {nom:'electroménager', image:'assets/Images/electromenager.jpg'}
+    ]
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
